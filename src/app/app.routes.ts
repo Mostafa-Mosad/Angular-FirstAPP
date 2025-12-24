9e0bc56acc7b93794626f6222c4122019916d256
Mostafa-Mosad <mostafaamosad@gmail.com>
2025-12-24 21:59:45 +0200

    Push firstApp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "product-list", component: ProductListComponent },
+  { path: "**", component: NotFoundComponent },
+];
